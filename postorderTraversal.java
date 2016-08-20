79fae5241a8baaf30d8d7fe5f231086b7b028465
qq783175223 <783175223@qq.com>
2016-08-20 16:52:01 +0800

    二叉树的后序遍历

---
+++ postorderTraversal.java
@@ -0,0 +1,36 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Postorder in ArrayList which contains node values.
+     */
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+        // write your code here
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        postorderTraversal2(root,list);
+        return list;
+    }
+    
+    public void postorderTraversal2(TreeNode root,ArrayList<Integer> list){
+        if(root==null){
+            return;
+        }
+        if(root.left==null&&root.right==null){
+            list.add(root.val);
+        }else{
+            postorderTraversal2(root.left,list);
+            postorderTraversal2(root.right,list);
+            list.add(root.val);
+        }
+    }
+}
