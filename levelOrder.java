685314d3b9a6a0ef7f56cf427d8c0df47cea91f2
qq783175223 <783175223@qq.com>
2016-08-26 14:26:52 +0800

    二叉树的层次遍历

--- levelOrder.java
+++ levelOrder.java
@@ -16,45 +16,36 @@ public class Solution {
      * @param root: The root of binary tree.
      * @return: Level order a list of lists of integer
      */
+    //二叉树的宽度优先算法
+    //一个队列搞定
     public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
         // write your code here
         ArrayList<ArrayList<Integer>> list=new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> listInside=null;
         if(root==null){
             return list;
         }
-        //将上一层的节点放入q1,下一层的节点放入q2
-        Queue<TreeNode> q1=new LinkedList<TreeNode>();
-        Queue<TreeNode> q2=new LinkedList<TreeNode>();
-        q1.add(root);
-        while(!q1.isEmpty()||!q2.isEmpty()){
-            ArrayList<Integer> listInside1=new ArrayList<Integer>();
-            ArrayList<Integer> listInside2=new ArrayList<Integer>();
-            while(!q1.isEmpty()){
-                TreeNode node=q1.poll();
-                listInside1.add(node.val);
+        Queue<TreeNode> q=new LinkedList<TreeNode>();
+        q.add(root);
+        int Qsize=1,tmp=0;//Qsize是每一个层的节点个数
+        while(!q.isEmpty()){
+            listInside=new ArrayList<Integer>();
+            while(Qsize-->0){
+                TreeNode node =q.poll();
+                listInside.add(node.val);
                 if(node.left!=null){
-                    q2.add(node.left);  
+                    q.add(node.left);
+                    tmp++;
                 }
                 if(node.right!=null){
-                    q2.add(node.right); 
+                    q.add(node.right);
+                    tmp++;
                 }
             }
-            if(listInside1.size()>0)
-                list.add(listInside1);
-            while(!q2.isEmpty()){
-                TreeNode node=q2.poll();
-                listInside2.add(node.val);
-                if(node.left!=null){
-                    q1.add(node.left);  
-                }
-                if(node.right!=null){
-                    q1.add(node.right); 
-                }
-            }
-            if(listInside2.size()>0)
-                list.add(listInside2);
+            Qsize=tmp;
+            tmp=0;
+            list.add(listInside);
         }
         return list;
     }
-
 }
