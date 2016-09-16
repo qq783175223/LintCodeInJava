a6d8b19e1590c1f455cc8ceba4a15b2a83b3f41d
qq783175223 <783175223@qq.com>
2016-09-16 11:20:44 +0800

    骰子求和

--- dices-sum.java
+++ dices-sum.java
@@ -1,36 +1,34 @@
 public class Solution {
-    private int g_maxVlaue=6;
     /**
      * @param n an integer
      * @return a list of Map.Entry<sum, probability>
      */
-    public List<Map.Entry<Integer, Double>> dicesSum(int n) {
+    public static List<Map.Entry<Integer, Double>> dicesSum(int n) {
         // Write your code here
         // Ps. new AbstractMap.SimpleEntry<Integer, Double>(sum, pro)
-        // to create the pair
-        List<Map.Entry<Integer, Double>> result=new ArrayList<Map.Entry<Integer, Double>>();
-        if(n<1){
-            return result;
-        }
-        double[][] probabilities=new double[2][n*g_maxVlaue+1];
+        // to create the pair.
+        List<Map.Entry<Integer, Double>> results = 
+                new ArrayList<Map.Entry<Integer, Double>>();
+        
+        double[][] probabilities = new double[2][6 * n + 1];
         int flag=0;
-        for(int i=1;i<=g_maxVlaue;i++){
-            probabilities[flag][i]=1;
-        }
-       flag=1;
-        for(int i=2;i<=n;i++){
-            for(int j=1;j<=i*g_maxVlaue;j++){
-                int p=j;
-                for(int k=0;k<6&&p>=1;k++){
-                    probabilities[flag][j] +=probabilities[1-flag][--p];
+        for (int i = 1; i <= 6; ++i)
+            probabilities[flag][i] = 1.0;
+
+        for (int i = 2; i <= n; ++i){
+            for (int j = 1; j <= 6 * i; ++j) {
+                probabilities[1-flag][j]=0.0;
+                for (int k = 1; k <= 6; ++k){
+                    if (j >= k)
+                        probabilities[1-flag][j] += probabilities[flag][j - k];
                 }
             }
             flag=1-flag;
         }
-        double total=Math.pow(g_maxVlaue,n);
-        for(int i=n;i<=n*g_maxVlaue;i++){
-            result.add(new AbstractMap.SimpleEntry<Integer, Double>(i,probabilities[1-flag][i]/total));
+        double total=Math.pow(6,n);
+        for (int i = n; i <= 6 * n; ++i){
+            results.add(new AbstractMap.SimpleEntry<Integer, Double>(i, probabilities[flag][i]/total));
         }
-        return result;
+        return results;
     }
 }
